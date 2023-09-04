5f27b6b4f718697a6a1dfbe8c8c1d4a0beedaad5
KiranPandranki17 <kiran02468@gmail.com>
2023-09-04 10:17:41 +0100

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./Components/CSS/proj.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Header">
+        <h1 className="Header1"> PHOTOGRAPHER </h1>
+        <h1> John Doe </h1>
+        <div class="btn-group">
+          <button>Home</button>
+          <button>Portfolio</button>
+          <button>Contact</button>
+          <button>Weddings</button>
+        </div>
+      </div>
+      <div className="photos">
+        <div className="photos-left">
+          <img src="https://www.w3schools.com/w3images/wedding.jpg" alt="#" />
+          <img src="https://www.w3schools.com/w3images/rocks.jpg" alt="#" />
+          <img src="https://www.w3schools.com/w3images/falls2.jpg" alt="#" />
+          <img src="https://www.w3schools.com/w3images/paris.jpg" alt="#" />
+          <img src="https://www.w3schools.com/w3images/nature.jpg" alt="#" />
+          <img src="https://www.w3schools.com/w3images/mist.jpg" alt="#" />
+          <img src="https://www.w3schools.com/w3images/paris.jpg" alt="#" />
+        </div>
+
+        <div className="photos-right">
+          <img src="https://www.w3schools.com/w3images/underwater.jpg" alt="#" />
+          <img src="https://www.w3schools.com/w3images/ocean.jpg" alt="#" />
+          <img src="https://www.w3schools.com/w3images/wedding.jpg" alt="#" />
+          <img src="https://www.w3schools.com/w3images/mountainskies.jpg" alt="#" />
+          <img src="https://www.w3schools.com/w3images/rocks.jpg" alt="#" />
+          <img src="https://www.w3schools.com/w3images/underwater.jpg" alt="#" />
+        </div>
+      </div>
+
+      <div className="footer">
+        <div className="logo">
+          <i class="fa fa-facebook-official"></i>
+          <i class="fa fa-instagram"></i>
+          <i class="fa fa-snapchat"></i>
+          <i class="fa fa-pinterest"></i>
+          <i class="fa fa-linkedin"></i>
+          <i class="fa fa-twitter"></i>
+        </div>
+      </div>
     </div>
   );
 }
